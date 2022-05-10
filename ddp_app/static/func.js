3164de80a9dcbbcd057dac288c2898af56bdb756
olaf michaelis <o.mic@web.de>
2022-05-10 12:55:30 +0200

    Add help text style and func js preparation

---
+++ ddp_app/static/func.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     sticky_section_titles();
+// });
+//
+// function sticky_section_titles() {
+//     $('.project-questions-form h2').next().css('background-color', 'orange');
+// }
